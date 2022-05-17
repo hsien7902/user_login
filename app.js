f8b8643ed256411287a9a3fd43a71af86b8156c5
hsien7902 <yuck7902@gmail.com>
2022-05-17 16:06:36 +0800

    feat: add mongoose connect

--- app.js
+++ app.js
@@ -2,11 +2,22 @@ const express = require('express')
 const app = express()
 const Port = 3000
 const exhbs = require('express-handlebars')
+const mongoose = require('mongoose')
 
+//mongoose connection
+mongoose.connect(process.env.MONGODB_URI)
+const db = mongoose.connection
+db.on('error', () => {
+  console.log('mongodb error')
+})
+db.once('open', () => {
+  console.log('mongodb connected')
+})
 
 //app.use
 app.engine('.hbs', exhbs.engine({ defaultLayout: 'main', extname: '.hbs' }))
 app.set('view engine', '.hbs')
+
 
 
 //Routers
